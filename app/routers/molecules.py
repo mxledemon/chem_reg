05faf97486a402cdeb2675b625e68327e04291b9
mxledemon <47308753+mxledemon@users.noreply.github.com>
2026-05-16 14:00:24 -0400

    Added duplicate handling for existance and SQLite conflict

--- app/routers/molecules.py
+++ app/routers/molecules.py
@@ -2,23 +2,51 @@ from fastapi import APIRouter, HTTPException
 from app.schemas import MoleculeCreate, MoleculeResponse, MoleculeUpdate
 from app.repositories import molecule_repository as mr
 from app.services.chemistry import derive_molecule_properties_from_smiles
+import sqlite3
 
 router = APIRouter(prefix='/molecules', tags=['molecules'])
 
 @router.post('', response_model=MoleculeResponse)
 def create_molecule(molecule: MoleculeCreate):
     molecule_data = molecule.model_dump()
+
     try:
-        derived_molecule_data = derive_molecule_properties_from_smiles(molecule_data['smiles'])
+        derived_molecule_data = derive_molecule_properties_from_smiles(
+            molecule_data['smiles']
+        )
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    
-    for key, value in derived_molecule_data.items():
-        molecule_data[key] = value
 
-    created_molecule = mr.create_molecule(molecule_data)
+    molecule_data.update(derived_molecule_data)
+
+    search_result = mr.find_molecule_by_inchikey(
+        derived_molecule_data['inchikey']
+    )
+
+    if search_result is not None:
+        raise HTTPException(
+            status_code=409,
+            detail={
+                "message": "Duplicate molecule detected",
+                "formula": derived_molecule_data["formula"],
+                "inchikey": derived_molecule_data["inchikey"],
+                "existing_molecule_id": search_result["id"],
+            },
+        )
+    try:
+        created_molecule = mr.create_molecule(molecule_data)
+    except sqlite3.IntegrityError as e:
+        existing_molecule = mr.find_molecule_by_inchikey(derived_molecule_data['inchikey'])
+        raise HTTPException(status_code=409, detail={
+            'message': 'Duplicate molecule detected',
+            'formula': derived_molecule_data['formula'],
+            'inchikey': derived_molecule_data['inchikey'],
+            'existing_molecule_id': existing_molecule['id'] if existing_molecule else None,
+        })
+
     if created_molecule is None:
         raise HTTPException(status_code=500, detail='Failed to create molecule')
+
     return created_molecule
 
 
